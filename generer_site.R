bc1f5a1f9245b90eebdc1e1cda71112e0b001ba0
OFB-IdF <128606748+OFB-IdF@users.noreply.github.com>
2024-06-06 10:31:32 +0000

    Deploying to gh-pages from @ OFB-IdF/src_idf@9bb4116f760f7f2edd311dbee4219ec782f77b5c 🚀

--- generer_site.R
+++ generer_site.R
@@ -1,5 +1,7 @@
 pak::pkg_install("OFB-IdF/FichesSRC")
 
+'%>%' <- dplyr::'%>%'
+
 FichesSRC::creer_toutes_fiches(
     fichier_info = "data-raw/metadata.xlsx",
     dossier_fiches = getwd(),
@@ -7,3 +9,10 @@ FichesSRC::creer_toutes_fiches(
     )
 
 quarto::quarto_render()
+
+readLines("_site/index.html") %>%
+    stringr::str_replace(
+        pattern = '<a href="./index.html" class="navbar-brand navbar-brand-logo">',
+        replacement = '<a href="https://www.ofb.gouv.fr" class="navbar-brand navbar-brand-logo">'
+    ) %>%
+    writeLines("_site/index.html", )
