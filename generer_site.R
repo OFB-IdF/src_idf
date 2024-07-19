e1e7ef188d2b306690782296eda3b42affbb9fb0
OFB-IdF <128606748+OFB-IdF@users.noreply.github.com>
2024-07-19 14:15:13 +0000

    Deploying to gh-pages from @ OFB-IdF/src_idf@bac2ebfd269aa191e55bc77b06e8c712d5fa7eed 🚀

--- generer_site.R
+++ generer_site.R
@@ -10,18 +10,4 @@ FichesSRC::creer_toutes_fiches(
 
 quarto::quarto_render(as_job = FALSE)
 
-list.files("_site", pattern = ".html", full.names = TRUE) %>%
-    purrr::walk(
-        .f = function(path) {
-            readLines(path) %>%
-    stringr::str_replace(
-        pattern = '<a href="./index.html" class="navbar-brand navbar-brand-logo">',
-        replacement = '<a href="https://www.ofb.gouv.fr" class="navbar-brand navbar-brand-logo">'
-    ) %>%
-                stringr::str_replace(
-                    pattern = '<a class="navbar-brand" href="./index.html">',
-                    replacement = '<a class="navbar-brand">'
-                ) %>%
-    writeLines(path)
-        }
-    )
+FichesSRC::ajuster_html(dossier = "_site")
