f56d4b198238cb64bbf440a2a099200f3202f74e
OFB-IdF <128606748+OFB-IdF@users.noreply.github.com>
2024-07-24 11:14:24 +0000

    Deploying to gh-pages from @ OFB-IdF/src_idf@6a2b1396fd5847c229608dc081d252e7421eeb6c 🚀

--- generer_site.R
+++ generer_site.R
@@ -2,6 +2,15 @@ pak::pkg_install("OFB-IdF/FichesSRC")
 
 '%>%' <- dplyr::'%>%'
 
+purrr::walk(
+    paste0("www/", c("history.png", "standing-up-man.png")),
+    function(x) {
+        file.copy(
+            from = system.file(x, package = "FichesSRC"),
+            to = x)
+    }
+)
+
 FichesSRC::creer_toutes_fiches(
     fichier_info = "data-raw/metadata.xlsx",
     dossier_fiches = getwd(),
