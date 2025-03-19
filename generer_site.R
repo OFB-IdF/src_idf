e98ffc21f3c0b3cfeeaaa9975ef3c4bc52d470a9
OFB-IdF <128606748+OFB-IdF@users.noreply.github.com>
2025-03-19 09:14:48 +0000

    Deploying to gh-pages from @ OFB-IdF/src_idf@4cccc49c206445faeed24d239e82a9a43f0dd6ec 🚀

--- generer_site.R
+++ generer_site.R
@@ -11,14 +11,43 @@ purrr::walk(
     }
 )
 
+metadata <- openxlsx2::read_xlsx("data-raw/metadata.xlsx") |>
+    dplyr::filter(publiable == "oui")
+
 FichesSRC::creer_toutes_fiches(
     fichier_info = "data-raw/metadata.xlsx",
     dossier_fiches = getwd(),
     region = 11
     )
 
-metadata <- openxlsx2::read_xlsx("data-raw/metadata.xlsx") |>
-    dplyr::filter(publiable == "oui")
+metadata$suivi |>
+    paste0(".qmd") |>
+    purrr::walk(
+        function(fiche) {
+            fiche_txt <- readLines(fiche)
+            if (all(!stringr::str_detect(
+                fiche_txt,
+                pattern = '- <script data-goatcounter="https://ofb-idf.goatcounter.com/count"
+                async src="//gc.zgo.at/count.js"></script>'
+            ))) {
+                fiche_txt |>
+                    sapply(
+                        function(ligne) {
+                            stringr::str_replace(
+                                ligne,
+                                pattern = 'include-in-header:',
+                                replacement = 'include-in-header:
+  - text: |
+        <script data-goatcounter="https://ofb-idf.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></script>'
+                )
+                        }
+                    )|>
+                    writeLines(con = fiche)
+            }
+
+            }
+    )
 
 brew::brew(
     file = "data-raw/index_template.qmd",
